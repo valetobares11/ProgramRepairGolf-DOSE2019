a09698c4f5e67fc20ee01c90199462d18b5af667
matias <mformento8@gmail.com>
2019-09-18 15:30:04 -0300

    corregi error en tabla

--- src/migrations/20190918142438_create_challenges_table.sql
+++ src/migrations/20190918142438_create_challenges_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS challenges(
     id integer auto_increment primary key,
     title varchar (50),
     description varchar(50),
-    source varchar(100000),
+    source varchar(10000),
     point integer,
     owner_id integer,
     owner_solution_id integer,
