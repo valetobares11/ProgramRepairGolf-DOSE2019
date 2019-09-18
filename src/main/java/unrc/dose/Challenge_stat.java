4fb798a44a033ac5583eb0ad985ef50f37bb043a
Nahuel Alvarez <nahuel.alvarez@smfconsulting.es>
2019-09-18 15:31:03 -0300

    #19 Challenge_stat java

---
+++ src/main/java/unrc/dose/Challenge_stat.java
@@ -0,0 +1,18 @@
+package unrc.dose;
+
+import org.javalite.activejdbc.Model;
+
+public class Challenge_stat extends Model {
+	public int getNumberOfSolutions() {
+		return 0;
+	}
+	
+	public int getNumberOfProposal() {
+		return 0;
+	}
+	
+	public int getSuccessRate() {
+		return 0;
+	}
+	
+}
