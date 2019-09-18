46ccb45031f14ef43010b975319146550aa366c1
Agustin <agustinborda1998@gmail.com>
2019-09-18 15:46:10 -0300

    Closes#19

--- src/main/java/unrc/dose/ChallengeStat.java
+++ src/main/java/unrc/dose/ChallengeStat.java
@@ -2,17 +2,20 @@ package unrc.dose;
 
 import org.javalite.activejdbc.Model;
 
-public class Challenge_stat extends Model {
+public class ChallengeStat extends Model {
 	public int getNumberOfSolutions() {
+		//TODO : Implement, this should return the number of valid solutions given to the challenge.
 		return 0;
 	}
-	
+
 	public int getNumberOfProposal() {
+		//TODO :
 		return 0;
 	}
-	
+
 	public int getSuccessRate() {
+		//TODO :
 		return 0;
 	}
-	
+
 }
