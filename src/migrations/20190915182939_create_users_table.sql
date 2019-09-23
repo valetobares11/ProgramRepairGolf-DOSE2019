c43172461e850bc8804f2530b3e79150e1c8d65d
alejandrobarrios <barriosalejandro792@gmail.com>
2019-09-23 19:50:28 -0300

    remove table users example

--- src/migrations/20190915182939_create_users_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (
-  id           INT(11) NOT NULL AUTO_INCREMENT,
-  username     VARCHAR(128),
-  password     VARCHAR(128),
-  created_at   DATETIME DEFAULT NULL,
-  updated_at   DATETIME DEFAULT NULL,
-  PRIMARY KEY (id)
-);
