063357cce7e7bd464292b751970df296f1d3c3d0
boaglio15 <agustinboaglio.95@gmail.com>
2019-09-23 16:44:23 -0300

    #21 add comment in class proposition

--- src/main/java/unrc/dose/Proposition.java
+++ src/main/java/unrc/dose/Proposition.java
@@ -2,6 +2,17 @@ package unrc.dose;
 
 import org.javalite.activejdbc.Model;
 
+/*
+* == Schema Info
+*
+* Table name: proposition
+*
+*  id              :integer(11)    not null, primary key
+*  user_id         :integet(11)    not null,
+*  challenge_id    :integer(11)    not null,
+*  source          :text
+*
+**/
 public class Proposition extends Model {
 
 }
