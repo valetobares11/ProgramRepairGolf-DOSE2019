2afe7cab99c646211a470854062482a8aabb73f6
mariana <mariana@mariana-HP-Laptop-14-bs0xx>
2019-09-23 16:20:42 -0300

    Creacion de tablas

--- src/migrations/Owner Solution.sql
+++ src/migrations/Owner Solution.sql
@@ -1,7 +1,10 @@
+Table name: Owner_Solutions
+
 create table if not exists Owner Solution (
 	id integer auto_increment primary key,
 	id_challenge integer,
 	user_id integer,
+	source varchar (10000),
 	created_at DATETIME,
 	updated_at DATETIME
 	);
