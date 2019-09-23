3fab2b086eb1016e6a97ba2b240f04121d7017b3
EriGoldenberg <erikagoldenberg@gmail.com>
2019-09-23 19:35:47 -0300

    agregamos schema comment

--- src/main/java/unrc/dose/Comment.java
+++ src/main/java/unrc/dose/Comment.java
@@ -1,3 +1,14 @@
+/*
+* == Schema Info
+*
+* Table name: comments
+*
+* id              :integer    not null, primary key
+* title           :varchar(50)    not null
+* description     :varchar(300)    not null
+* challenge_id    :integer     not null
+* user_id         :integer    not null
+**/
 package unrc.dose;
 
 import org.javalite.activejdbc.Model;
