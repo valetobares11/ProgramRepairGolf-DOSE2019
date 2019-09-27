9578d902d28ca50b9c950bf4d964718a9d18004c
cadafe <cadafe12@gmail.com>
2019-09-27 20:45:35 -0300

    db table challenge_stats (+2 attributes)

--- src/migrations/20190918143814_create_challenge_stats_table.sql
+++ src/migrations/20190918143814_create_challenge_stats_table.sql
@@ -1,6 +1,11 @@
 CREATE TABLE IF NOT EXISTS challenge_stats(
-    id integer auto_increment primary key,
-    challenge_id integer not null,
+    id INTEGER auto_increment primary key,
+    challenge_id INTEGER not null,
+    solved_count INTEGER,
+    average_score INTEGER,
     created_at DATETIME,
-	  updated_at DATETIME
+	updated_at DATETIME
 );
+
+
+
