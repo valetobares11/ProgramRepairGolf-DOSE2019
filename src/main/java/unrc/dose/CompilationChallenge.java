484e90a73968b368784bbc560fa03d10e7e7ebf6
matias <mformento8@gmail.com>
2019-09-30 20:51:36 -0300

    add tables and models

--- src/main/java/unrc/dose/CompilationChallenge.java
+++ src/main/java/unrc/dose/CompilationChallenge.java
@@ -5,15 +5,15 @@ import org.javalite.activejdbc.Model;
 /*	
 	Atributos de la tabla:
 		id: integer,
-		user_id: integer,
+		user_id :integer,
 		title :varchar (50),
 		description :varchar(50),
 		source :varchar(100000),
 		point :integer,
-		owner_id :integer,
+		owner_:id integer,
 		owner_solution_id :integer
 */
-public class Challenge extends Model {
+public class CompilationChallenge extends Model {
 
 
 }
--- src/main/java/unrc/dose/TestChallenge.java
+++ src/main/java/unrc/dose/TestChallenge.java
@@ -5,15 +5,15 @@ import org.javalite.activejdbc.Model;
 /*	
 	Atributos de la tabla:
 		id: integer,
-		user_id: integer,
+		user_id :integer,
 		title :varchar (50),
 		description :varchar(50),
 		source :varchar(100000),
 		point :integer,
 		owner_id :integer,
 		owner_solution_id :integer
+		test :varchar(1000)
 */
-public class Challenge extends Model {
-
+public class TestChallenge extends Model {
 
 }
