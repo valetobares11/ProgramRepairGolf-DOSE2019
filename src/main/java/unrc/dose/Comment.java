2b742dd83b5d5d64b45ca34f98cb5777c1ef1442
EriGoldenberg <erikagoldenberg@gmail.com>
2019-09-30 19:38:31 -0300

    Creacion del metodo para crear un comentario

--- src/main/java/unrc/dose/Comment.java
+++ src/main/java/unrc/dose/Comment.java
@@ -1,3 +1,7 @@
+package unrc.dose;
+
+import org.javalite.activejdbc.Model;
+
 /*
 * == Schema Info
 *
@@ -8,10 +12,27 @@
 * description     :varchar(300)    not null
 * challenge_id    :integer     not null
 * user_id         :integer    not null
+* comment_id      :integer
 **/
-package unrc.dose;
-
-import org.javalite.activejdbc.Model;
-
+/**
+*@author Celi Yanina, de Prada Hernan, Goldenberg Erika
+**/
 public class Comment extends Model {
+
+  /**
+  *Create a new comment.
+  *@param title this is the comment's title
+  *@param description this the comment's body
+  *@param challenge_id this is the challenge's id which is commented
+  *@param user_id this is the user's id who commented
+  *@return Nothing.
+  **/
+  public void createComment (String title,String description, int challenge_id, int user_id ){
+    this.set("title",title);
+    this.set("description",description);
+    this.set("challenge_id", challenge_id);
+    this.set("user_id", user_id);
+    this.saveIt();
+  }
+
 }
