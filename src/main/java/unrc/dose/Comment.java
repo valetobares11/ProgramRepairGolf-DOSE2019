6d58de7e7819995b835ea74f44acfd5b30bf7cd5
EriGoldenberg <erikagoldenberg@gmail.com>
2019-10-02 16:02:20 -0300

    Arreglos de javadoc

--- src/main/java/unrc/dose/Comment.java
+++ src/main/java/unrc/dose/Comment.java
@@ -26,17 +26,7 @@ public class Comment extends Model {
   *@param challenge_id this is the challenge's id which is commented
   *@param user_id this is the user's id who commented
   **/
-  /**
-  *Create a new Response.
-  *@param description this the response's body
-  *@param challenge_id this is the challenge's id which is commented
-  *@param user_id this is the user's id who responded to a comment
-  **/
-  /**
-  *isResponse
-  *@param comment_id this is comments or response to evaluate
-  *@return return if the selected comment_id si a comment or a response.
-  **/
+
   public void createComment (String title,String description, int challenge_id, int user_id ){
     this.set("title",title);
     this.set("description",description);
@@ -44,12 +34,21 @@ public class Comment extends Model {
     this.set("user_id", user_id);
     this.saveIt();
   }
-
+  /*
+  *isResponse
+  *@param comment_id this is comments or response to evaluate
+  *@return return if the selected comment_id si a comment or a response.
+  **/
   public boolean isResponse(int comment_id){
     Comment c = Comment.findById(comment_id);
     return ((c.getInteger("comment_id")) != (null));
   }
-
+  /**
+  *Create a new Response.
+  *@param description this the response's body
+  *@param challenge_id this is the challenge's id which is commented
+  *@param user_id this is the user's id who responded to a comment
+  **/
   public void createResponse (String description, int user_id, int comment_id) throws NullPointerException{
     if (!isResponse(comment_id)){
     Comment c = Comment.findById(comment_id);
