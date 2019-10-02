a4e18744a5803e222b1fe0c50ce561395ad75473
cadafe <cadafe12@gmail.com>
2019-10-02 15:24:58 -0300

    new migration:alter table challenge_stats

--- src/migrations/20190918143814_create_challenge_stats_table.sql
+++ src/migrations/20190918143814_create_challenge_stats_table.sql
@@ -1,8 +1,6 @@
 CREATE TABLE IF NOT EXISTS challenge_stats(
     id INTEGER auto_increment primary key,
     challenge_id INTEGER not null,
-    solved_count INTEGER,
-    average_score INTEGER,
     created_at DATETIME,
 	updated_at DATETIME
 );
---
+++ src/migrations/20191002151255_alter_table_challenge_stats.sql
@@ -0,0 +1,5 @@
+ALTER TABLE challenge_stats 
+ADD average_score INTEGER;
+
+ALTER TABLE challenge_stats 
+ADD solved_count INTEGER;
