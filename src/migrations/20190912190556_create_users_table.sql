50952d84bf2625d21ede4ca3b0ba1e171ac05549
maxigaspero <maxigaspero@gmail.com>
2019-10-07 18:46:59 -0300

    test

--- src/migrations/20190912190556_create_users_table.sql
+++ src/migrations/20190912190556_create_users_table.sql
@@ -4,7 +4,8 @@ CREATE TABLE IF NOT EXISTS users(
     password varchar(20) not null,
     email_address varchar(50) not null,
     admin BOOLEAN not null default 0,
-    active_account BOOLEAN not null default 1,
     created_at DATETIME,
-    updated_at DATETIME
+    updated_at DATETIME,
+    unique(username),
+    unique(email_address)
 );
--- src/test/java/unrc/dose/UserTest.java
+++ src/test/java/unrc/dose/UserTest.java
@@ -24,13 +24,75 @@ public class UserTest {
       	Base.open();
   	}
 
+
   	@AfterClass
   	public static void tearDown() {
     	Spark.stop();
     	Base.close();
   	}
 
-  	/**
+    /**
+    * found a user by his username succesfully
+    * @result user found 
+    */
+	@Test
+	public void userFoundByName() {
+		String name = "JohnConnor";
+
+		assertEquals(false , User.searchUserByUsername(name));
+	}
+
+    /**
+    * user not found by his username
+    * @result user not found  
+    */
+	@Test
+	public void userNotFoundByName() {
+		String name = "Pity";
+
+		assertEquals(true , User.searchUserByUsername(name));
+	}
+
+    /**
+    * found a user by his email_address succesfully 
+    * @result email_addres found  
+    */
+	@Test
+	public void userFoundByEmail() {
+		String email = "JohnConnor@gmail.com";
+
+		assertEquals(false , User.searchUserByEmail(email));
+	}
+
+    /**
+    * user not found by his email_address
+    * @result email_addres not found  
+    */
+	@Test
+	public void userNotFoundByEmail() {
+		String email = "YvaelTercero@gmail.com";
+
+		assertEquals(true , User.searchUserByEmail(email));
+	}
+
+    /**
+    * load all the fields of a user with his data
+    * @result user charged with fields that have passed by the user 
+    */
+	@Test
+	public void userSet() {
+		String name = "Enzo";
+		String password = "Ferrari";
+		String email = "F40@gmail.com";
+		Boolean admin = false;
+		User pepe = new User();
+
+		pepe = User.set(name, password, email, admin);	
+
+		assertEquals(name , pepe.get("username"));
+	}
+
+	/**
 	 * delete an user
 	 * @result delete logically a user logged
 	 */
@@ -39,7 +101,7 @@ public class UserTest {
 		String username = "JohnConnor";
 		String password = "VeryHardPass";
 
-		assertEquals("Usuario eliminado",User.deleteUser(username, password));
+		assertEquals(true, User.deleteUser(username, password));
 	}
 
 	/**
@@ -51,9 +113,8 @@ public class UserTest {
 		String username = "Pity";
 		String password = "Martinez";
 
-		assertEquals("Usuario no encontrado",User.deleteUser(username, password));
+		assertEquals(false, User.deleteUser(username, password));
 	}
+
 }
-
-
 
