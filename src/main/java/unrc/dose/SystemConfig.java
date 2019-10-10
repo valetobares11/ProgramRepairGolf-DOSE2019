4330f3c17fcb9771e34433add9edde287daabdbb
elismer <elismerbongiovanni@hotmail.com>
2019-10-10 19:55:15 -0300

    Style checked in SystemConfig

--- src/main/java/unrc/dose/SystemConfig.java
+++ src/main/java/unrc/dose/SystemConfig.java
@@ -4,23 +4,30 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Class for system config framework based on Singleton
- * 
- * @author Elismer Bongiovanni
+ * Class for system config framework based on Singleton.
+ *
  * @version 0.1.0
  */
-public class SystemConfig {
-  /** int containing the port to be listening */
-  private int port = 4567;
-  /** Object containing the instance to be returned */
+public final class SystemConfig {
+
+  /** int define the defualt port for spark. */
+  private static final int DEFAULT_PORT = 4567;
+
+  /** int containing the port to be listening. */
+  private int port = DEFAULT_PORT;
+
+  /** Object containing the instance to be returned. */
   private static SystemConfig singleton;
 
+  /**
+   * Defualt constructor. This is private, bacuse is designed as a singleton.
+   */
   private SystemConfig() {
   }
 
   /**
-   * This return a instance of class
-   * 
+   * This return a instance of class.
+   *
    * @return a Singleton whit the port that will be used
    */
   public static SystemConfig getInstance() {
@@ -31,17 +38,17 @@ public class SystemConfig {
   }
 
   /**
-   * This set a port to be listen
-   * 
-   * @param portToLisen
+   * This set a port to be listen.
+   *
+   * @param portToListen the new port to listen
    */
-  public void setPort(int portToListen) {
+  public void setPort(final int portToListen) {
     this.port = portToListen;
   }
 
   /**
-   * This retunr the port that is being listening
-   * 
+   * This retunr the port that is being listening.
+   *
    * @return the port
    */
   public int getPort() {
@@ -50,7 +57,9 @@ public class SystemConfig {
 
   /**
    * We overwrite the clone method, so that an object of this class cannot be
-   * cloned
+   * cloned.
+   *
+   * @return a CloneNotSupportedException
    */
   @Override
   public SystemConfig clone() {
