c0c9c296779865286189ed07d1b0396494730951
Agustin <agustinborda1998@gmail.com>
2019-10-13 01:08:38 -0300

    Removed non-used imports

--- src/test/java/unrc/dose/UserStatTest.java
+++ src/test/java/unrc/dose/UserStatTest.java
@@ -7,9 +7,7 @@ import static org.junit.Assert.assertTrue;
 
 import org.javalite.activejdbc.LazyList;
 import org.javalite.activejdbc.Base;
-import org.junit.After;
 import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
