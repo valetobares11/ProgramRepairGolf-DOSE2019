dfd5892667d45b49b850d84446e9e6bdd5bdb596
Franco Brusatti <fbrusatti@gmail.com>
2019-10-14 22:31:49 -0300

    Create basic swagger example using Belly infrastructure

--- src/main/java/unrc/dose/Belly.java
+++ src/main/java/unrc/dose/Belly.java
@@ -1,17 +1,45 @@
+/**
+ * This is the main project package
+ *
+ * @author franco
+ * @version 0.1
+ */
 package unrc.dose;
 
 /** This is just an example class. */
 public class Belly {
-  private static final int capacity = 40;
+  /** Belly's capacity. */
+  private static final int CAPACITY = 40;
+
   /** keeps the value of the current level of elements into Belly. */
   private int current;
 
-  public void eat(int cukes) {
+  /** Belly identifier. */
+  private int id;
+
+  /** Belly's owner. */
+  private String name;
+
+  /** Constructor.
+   * @param id Identifier
+   * @param name slug
+  */
+  public Belly(final int id, final String name) {
+    this.id = id;
+    this.name = name;
+  }
+
+  /** Adds cukes into belly.
+   * @param cukes int
+   */
+  public void eat(final int cukes) {
     current = cukes;
   }
 
+  /** Checks if belly is full.
+   * @return boolean
+   */
   public boolean isFull() {
-    return capacity <= current;
+    return CAPACITY <= current;
   }
-
 }
