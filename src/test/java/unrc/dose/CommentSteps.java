a8ddbd052f00fd4576ffddd12fdb2dd883d5cf4c
hernan-dp <h.de.prada1@gmail.com>
2019-10-31 00:31:17 -0300

    Steps

--- src/test/java/unrc/dose/CommentSteps.java
+++ src/test/java/unrc/dose/CommentSteps.java
@@ -5,65 +5,79 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import cucumber.api.PendingException;
 import static org.junit.Assert.*;
-
+import unrc.dose.Comment;
 import java.util.HashMap;
 import java.util.Map;
-
+import unrc.dose.Challenge;
 import unrc.dose.User;
 import unrc.dose.StepUtils;
-
 import org.javalite.activejdbc.LazyList;
 
 public class CommentSteps extends StepUtils {
-  Comment c = new Comment();
-  Comment r = new Comment();
-  User u = new User();
-  @When("^the user writes the title \"([^\"]*)\" and the description \"([^\"]*)\"$")
+User u = new User();
+Comment c = new Comment();
+Challenge ch = new Challenge();
+
+@Given("^the user \"([^\"]*)\" with id (\\d+) is already logged on$")
+public void the_user_with_id_is_already_logged_on(String arg1, int arg2) throws Exception {
+    u = User.findById(arg2);
+}
+
+@Given("^the challenge with id (\\d+) exists$")
+public void the_challenge_with_id_exists(int arg1) throws Exception {
+    ch= Challenge.findById(arg1);
+}
+
+@When("^the user writes the title \"([^\"]*)\" and the description \"([^\"]*)\"$")
 public void the_user_writes_the_title_and_the_description(String arg1, String arg2) throws Exception {
+    /*c.set("user_id", u.getInteger("id"));
+    c.set("challenge_id", ch.getInteger("id"));*/
     c.set("title", arg1);
     c.set("description", arg2);
 }
+
 
 @Then("^the system will saves the comment$")
 public void the_system_will_saves_the_comment() throws Exception {
     c.saveIt();
 }
 
-@Given("^the comment already exists$")
-public void the_comment_already_exists() throws Exception {
-    assertNotNull(c);
+@Given("^the comment with id (\\d+) already exists$")
+public void the_comment_with_id_already_exists(int arg1) throws Exception {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
 }
 
-@When("^the user writes the title \"([^\"]*)\" of the response and the description \"([^\"]*)\" to the comment with id (\\d+)$")
-public void the_user_writes_the_title_of_the_response_and_the_description_to_the_comment_with_id(String arg1, String arg2, int arg3) throws Exception {
-  r.set("title", arg1);
-  r.set("description", arg2);
-  r.set("comment_id", arg3);
+@When("^the user writes the title \"([^\"]*)\" of the response and the description \"([^\"]*)\"$")
+public void the_user_writes_the_title_of_the_response_and_the_description(String arg1, String arg2) throws Exception {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
 }
-
 
 @Then("^the system will save the response$")
 public void the_system_will_save_the_response() throws Exception {
-    r.saveIt();
-}
-
-@Given("^the user \"([^\"]*)\" exists$")
-public void the_user_exists(String arg1) throws Exception {
     // Write code here that turns the phrase above into concrete actions
     throw new PendingException();
 }
 
-@When("^the user wants to see the comments \"([^\"]*)\"$")
-public void the_user_wants_to_see_the_comments(String arg1) throws Exception {
+@Given("^the user \"([^\"]*)\" with id (\\d+) exists$")
+public void the_user_with_id_exists(String arg1, int arg2) throws Exception {
     // Write code here that turns the phrase above into concrete actions
     throw new PendingException();
 }
 
-@Then("^the system will return a list of comments of the user$")
-public void the_system_will_return_a_list_of_comments_of_the_user() throws Exception {
+@When("^the user \"([^\"]*)\" wants to see the comments of the user \"([^\"]*)\"$")
+public void the_user_wants_to_see_the_comments_of_the_user(String arg1, String arg2) throws Exception {
     // Write code here that turns the phrase above into concrete actions
     throw new PendingException();
 }
+
+@Then("^the system will return a list of comments of the user \"([^\"]*)\"$")
+public void the_system_will_return_a_list_of_comments_of_the_user(String arg1) throws Exception {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
 
 @When("^the user wants to see the comments$")
 public void the_user_wants_to_see_the_comments() throws Exception {
@@ -77,8 +91,8 @@ public void the_system_will_return_a_list_of_comments_of_the_challenge() throws 
     throw new PendingException();
 }
 
-@Given("^the comment exists$")
-public void the_comment_exists() throws Exception {
+@Given("^the comment with id (\\d+) exists$")
+public void the_comment_with_id_exists(int arg1) throws Exception {
     // Write code here that turns the phrase above into concrete actions
     throw new PendingException();
 }
@@ -94,7 +108,5 @@ public void the_system_will_return_a_list_of_responses_to_the_comment() throws E
     // Write code here that turns the phrase above into concrete actions
     throw new PendingException();
 }
-
-
 
 }
--- src/test/resources/unrc/dose/comments.feature
+++ src/test/resources/unrc/dose/comments.feature
@@ -1,30 +1,30 @@
 Feature: comments
       Scenario: User wants to create a new comment to a challenge
-        Given the user "Pablo" is already logged on
-        And the challenge exists
+        Given the user "Pablo" with id 1 is already logged on
+        And the challenge with id 1 exists
         When the user writes the title "titleEx" and the description "descriptionEx"
         Then the system will saves the comment
 
       Scenario: User wants to create a new response to a commment
-        Given the user "Pedro" is already logged on
-        And the comment already exists
-        When the user writes the title "titleEx" of the response and the description "descriptionEx" to the comment with id 1
+        Given the user "Pedro" with id 1 is already logged on
+        And the comment with id 1 already exists
+        When the user writes the title "titleEx" of the response and the description "descriptionEx"
         Then the system will save the response
 
       Scenario: User wants to see the comments by another user
-        Given the user "Pablo" is already logged on
-        And the user "Pedro" exists
-        When the user wants to see the comments "comment1"
-        Then the system will return a list of comments of the user
+        Given the user "Pablo" with id 1 is already logged on
+        And the user "Pedro" with id 2 exists
+        When the user "Pablo" wants to see the comments of the user "Pedro"
+        Then the system will return a list of comments of the user "Pedro"
 
       Scenario: User wants to see the comments of a challenge
         Given the user "Pablo" is already logged on
-        And the challenge exists
+        And the challenge with id 1 exists
         When the user wants to see the comments
         Then the system will return a list of comments of the challenge
 
-      Scenario: User wants to see the comments of a challenge
+      Scenario: User wants to see the response of a comment
         Given the user "Pablo" is already logged on
-        And the comment exists
+        And the comment with id 1 exists
         When the user wants to see the responses of that comment
         Then the system will return a list of responses to the comment
